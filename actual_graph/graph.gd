74e1e6e3b55627c682edbfebc83cd97913eb5621
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2024-01-13 13:22:39 -0700

    graph-pinkblocks-nowave

---
+++ actual_graph/graph.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@export var point_prefab: PackedScene
+@export var resolution = 10
+
+var points = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var step = 2.0 / resolution
+	var position = Vector3.ZERO
+	var scale = Vector3.ONE * step
+	
+	for i in range(resolution):
+		var point = point_prefab.instantiate()
+		points.push_back(point)
+		position.x = (i + 0.5) * step - 1.0
+		point.position = position
+		point.scale = scale
+		add_child(point)
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+var speed = 1.0
+func _process(delta):
+	var time = Time.get_ticks_msec() / 1000
+	for point in points:
+		position = point.position
+		position.y = sin(PI * (position.x * time))
+		point.position = position
