abf52abe3915cd02a3975467a3460522b8488520
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2024-03-01 21:40:05 -0700

    We have a functional morph graph baby

--- actual_graph/scripts/graph.gd
+++ actual_graph/scripts/graph.gd
@@ -1,31 +1,110 @@
 extends Node3D
 
 @export var point_prefab: PackedScene
-@export var resolution = 100
-
+@export var resolution = 35
+var function: FUNCTION = 0
+var transitionFunction: FUNCTION = 0
+@export var functionDuration = 5
+@export var transitionDuration = 2
+var transitioning = false
 var points = []
 
-# Called when the node enters the scene tree for the first time.
+enum FUNCTION { SPHERE, TORUS, WAVE, MULTIWAVE, RIPPLE }
+
 func _ready():
 	var step = 2.0 / resolution
-	var position = Vector3.ZERO
-	var scale = Vector3.ONE * step
-	
-	for i in range(resolution):
+	var pointScale = Vector3.ONE * step
+	points = []  # Initialize as an empty array
+	for i in range(resolution * resolution):
 		var point = point_prefab.instantiate()
-		points.push_back(point)
-		position.x = (i + 0.5) * step - 1.0
-		point.position = position
-		point.scale = scale
+		points.append(point)  # Add each point to the array
 		add_child(point)
-		
+		point.scale = pointScale
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 var speed = 1.0
-var time = 0
+var elapsedTime = 0
+var duration = 0
 func _process(delta):
-	time += delta
-	for point in points:
-		position = point.position
-		position.y = GraphFunctions.multiwave(position.x, time)
-		point.position = position
+	elapsedTime += delta
+	duration += delta
+	if transitioning:
+		if duration >= transitionDuration:
+			duration -= transitionDuration
+			transitioning = false
+	elif duration >= functionDuration:
+		duration -= functionDuration
+		transitioning = true
+		transitionFunction = function
+		function = get_next_function(function)
+
+	if transitioning:
+		update_function_transition()
+	else:
+		update_function()
+	
+func get_next_function(name: FUNCTION):
+	if name == FUNCTION.SPHERE:
+		name = FUNCTION.TORUS
+	elif name == FUNCTION.TORUS:
+		name = FUNCTION.WAVE
+	elif name == FUNCTION.WAVE:
+		name = FUNCTION.MULTIWAVE
+	elif name == FUNCTION.MULTIWAVE:
+		name = FUNCTION.RIPPLE
+	else:
+		name = FUNCTION.SPHERE
+	return name
+	
+func morph(u, v, progress):
+	var pos1 = do_calculation_v2(u, v, transitionFunction)
+	var pos2 = do_calculation_v2(u, v, function)
+	return lerp(pos1, pos2, smoothstep(0,1,progress))
+	
+func update_function_transition():
+	var step = 2.0 / resolution
+	var i = 0
+	var x = 0
+	var z = 0
+	var v = 0.5 * step - 1.0
+	var progress = duration / transitionDuration
+	while i < points.size():
+		if x == resolution:
+			x = 0
+			z += 1
+			v = (z + 0.5) * step - 1.0
+		var u = (x + 0.5) * step - 1.0
+		points[i].position = morph(u, v, progress)
+		i += 1
+		x += 1
+		
+func update_function():
+	var step = 2.0 / resolution
+	var i = 0
+	var x = 0
+	var z = 0
+	var v = 0.5 * step - 1.0
+
+	while i < points.size():
+		if x == resolution:
+			x = 0
+			z += 1
+			v = (z + 0.5) * step - 1.0
+		var u = (x + 0.5) * step - 1.0
+		points[i].position = do_calculation_v2(u, v, function)
+		i += 1
+		x += 1
+
+func do_calculation_v2(u, v, name):
+	var pos = Vector3.ZERO
+	match name:
+		FUNCTION.SPHERE:
+			pos = GraphFunctions.sphere(u, v, elapsedTime)
+		FUNCTION.TORUS:
+			pos = GraphFunctions.torus(u, v, elapsedTime)
+		FUNCTION.WAVE:
+			pos = GraphFunctions.wave(u, v, elapsedTime)
+		FUNCTION.MULTIWAVE:
+			pos = GraphFunctions.multiwave(u, v, elapsedTime)
+		FUNCTION.RIPPLE:
+			pos = GraphFunctions.ripple(u, v, elapsedTime)
+	return pos
--- actual_graph/scripts/graph_functions.gd
+++ actual_graph/scripts/graph_functions.gd
@@ -1,10 +1,47 @@
 
 class_name GraphFunctions
 
-static func wave(x, t):
-	return sin(PI * (x+t))
+static func wave(u, v, t):
+	var p = Vector3.ZERO
+	p.x = u
+	p.y = sin(PI * (u + v + t))
+	p.z = v
+	return p
 
-static func multiwave(x, t):
-	var y = sin(PI * (x + 0.5 * t))
-	y += 0.5 * sin(2.0 * PI * (x + t))
-	return y
+static func multiwave(u, v, t):
+	var p = Vector3.ZERO
+	p.x = u
+	p.y = sin(PI * (u + 0.5 * t))
+	p.y += 0.5 * sin(2.0 * PI * (v + t))
+	p.y += sin(PI * (u + v + 0.25 * t))
+	p.y *= 1.0 / 2.5
+	p.z = v
+	return p
+
+static func ripple(u, v, t):
+	var d = sqrt(u * u + v * v)
+	var p = Vector3.ZERO
+	p.x = u
+	p.y = sin(PI * (4.0 * d - t))
+	p.y /= 1.0 + 10.0 * d
+	p.z = v
+	return p
+	
+static func sphere(u, v, t):
+	var pos = Vector3.ZERO
+	var r = 0.9 + 0.1 * sin(PI * (6.0 * u + 4.0 * v + t))
+	var s = r * cos(0.5 * PI * v)
+	pos.x = s * sin(PI * u)
+	pos.y = r * sin(PI * 0.5 * v)
+	pos.z = s * cos(PI * u)
+	return pos
+
+static func torus(u, v, t):
+	var pos = Vector3.ZERO
+	var r1 = 0.7 + 0.1 * sin(PI * (6.0 * u + 0.5 * t))
+	var r2 = 0.15 + 0.05 * sin(PI * (8 * u + 4 * v + 2 * t))
+	var s = r1 + r2 * cos(PI * v)
+	pos.x = s * sin(PI * u)
+	pos.y = r2 * sin(PI * v)
+	pos.z = s * cos(PI * u)
+	return pos
